4108466906570c54480b71edee520309c466794b
kazukk <kazuhiro.shimizu@gmail.com>
2015-06-18 14:25:53 +0900

    stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,8 +2,8 @@ Rails.configuration.stripe = {
  # :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
  # :secret_key      => ENV['STRIPE_SECRET_KEY']
 
- secret_key      => "sk_test_mHvznSn71GM6WGi2n65E3Bfa",
- publishable_key => "pk_test_XJ67QZZnnwyCJW2TRVow3UPN"
+ publishable_key => "pk_test_XJ67QZZnnwyCJW2TRVow3UPN",
+ secret_key      => "sk_test_mHvznSn71GM6WGi2n65E3Bfa"
 
 
 }
